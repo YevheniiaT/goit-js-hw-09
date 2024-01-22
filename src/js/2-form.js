c4a6a5fd6aa517acf35e0afeeaeae566a15456d7
YevheniiaT <jane1talala@gmail.com>
2024-01-22 18:40:44 +0200

    add changes form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,10 +1,10 @@
 const form = document.querySelector("form");
 
 
-let LOCAL_STORAGE_KEY = "feedback-form-state"
+const LOCAL_STORAGE_KEY = "feedback-form-state"
 
 let dataForm = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {}
-console.log(dataForm)
+
 form.addEventListener('input', () => {
      const { email, message } = form.elements;
   dataForm = { email: email.value.trim(), message: message.value.trim() };
@@ -22,17 +22,21 @@ function initFormVal() {
 
 initFormVal()
 
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const { email, message } = form.elements;
   
-    console.log({ email: email.value.trim(), message: message.value.trim() })
-    
-    localStorage.removeItem(LOCAL_STORAGE_KEY);
-   
-//      const { email, message } = form.elements;
-//   dataForm = { email: email.value.trim(), message: message.value.trim() };
-//       localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(dataForm))
-    
+    const trimmedEmail = email.value;
+    const trimmedMessage = message.value;
+  
+    if (trimmedEmail !== '' && trimmedMessage !== '') {
+        console.log({ email: trimmedEmail, message: trimmedMessage });
+        localStorage.removeItem(LOCAL_STORAGE_KEY);
+    } else {
+      alert('Fill in all fields of the form');
+    }
 });
+
+
 
