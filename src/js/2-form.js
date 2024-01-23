189e18090d61c08390f445c7b3049f80e0496124
YevheniiaT <jane1talala@gmail.com>
2024-01-23 21:43:02 +0200

    add form chng

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,16 +1,22 @@
 const form = document.querySelector("form");
-
-
 const LOCAL_STORAGE_KEY = "feedback-form-state"
 
-let dataForm = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {}
+let dataForm = null
+try {
+ dataForm = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {}
+}
+catch (error) {
+  console.log(error.name)
+}
+
 
 form.addEventListener('input', () => {
      const { email, message } = form.elements;
-  dataForm = { email: email.value.trim(), message: message.value.trim() };
-      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(dataForm))
-    
+  dataForm.email = email.value;
+  dataForm.message = message.value;
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(dataForm))
 });
+
 
 function initFormVal() {
   const { email, message } = form.elements;
@@ -27,11 +33,11 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
     const { email, message } = form.elements;
   
-    const trimmedEmail = email.value;
-    const trimmedMessage = message.value;
+    const emailEl = email.value.trim();
+    const messageEl = message.value.trim();
   
-    if (trimmedEmail !== '' && trimmedMessage !== '') {
-        console.log({ email: trimmedEmail, message: trimmedMessage });
+    if (emailEl !== '' && messageEl !== '') {
+        console.log({ email: emailEl, message: messageEl });
         localStorage.removeItem(LOCAL_STORAGE_KEY);
     } else {
       alert('Fill in all fields of the form');
